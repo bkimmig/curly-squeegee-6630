5fb4c45633b24180a1c4a99c58a16773d4655f6a
Brian Kimmig <brian.kimmig@gmail.com>
2015-11-03 14:27:51 -0700

    added the movies collection

--- client/js/lib/subscriptions.js
+++ client/js/lib/subscriptions.js
@@ -1,2 +1,3 @@
 
 Meteor.subscribe("actors");
+Meteor.subscribe("movies");
--- server/lib/publications.js
+++ server/lib/publications.js
@@ -2,3 +2,7 @@
 Meteor.publish('actors', function() {
     return Actors.find();
 });
+
+Meteor.publish('movies', function() {
+    return Movies.find();
+});
