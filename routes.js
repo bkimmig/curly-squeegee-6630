a2487066d5888b6f8df925dadd9325c2479c340d
Brian Kimmig <brian.kimmig@gmail.com>
2015-11-09 21:56:11 -0700

    added loading screen template and route

--- routes.js
+++ routes.js
@@ -16,6 +16,10 @@ Router.route('/hello', {
     template: 'hello',
 });
 
+Router.route('/loading', {
+    template: 'loading',
+});
+
 /*Router.route('/search', function() {
     this.render('searchApiFilms');
 });*/
