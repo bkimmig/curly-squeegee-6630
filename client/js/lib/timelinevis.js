2c3234c66bea84e9c497ffa1a9e69527982b2dc4
Jimmy Moore <jimmy@cs.utah.edu>
2015-12-02 15:39:45 -0700

    tweaked tool tip information, included posters.
    Still needs to resize

--- client/js/lib/timelinevis.js
+++ client/js/lib/timelinevis.js
@@ -4,6 +4,8 @@
 
 
 TimeLineVis = function(_parentElement, _session) {
+
+    //copy global data to be visible to this function
     var self = this;
 
      self.timelineVisKeys = [
@@ -33,28 +35,22 @@ TimeLineVis.prototype.initVis = function () {
     //copy global data to be visible to this function
     var self = this; 
 
+    // var tip = d3.tip()
+    //     .data(self.data)
+    //     .attr('class', 'd3-tip')
+    //     .offset([-10, 0])
+    //     .html(function(d) {
+    //         return "<strong>Title:</strong> <span style='color:red'>" + d.Title+ "</span>";
+    //     })
 
     //add tool tip for mouseover: https://gist.github.com/mstanaland/6100713
      var div = self.parentElement.append("div")
         .attr("class", "tooltip-tl")
         .style("display", "none");
 
- 
 
-        // div.append("rect")
-        //   .attr("width", 20)
-        //   .attr("height", 20)
-        //   .attr("fill", "white")
-        //   .style("opacity", 0.5);
 
-        // div.append("text")
-        //   .attr("x", 15)
-        //   .attr("dy", "1.2em")
-        //   .style("text-anchor", "middle")
-        //   .attr("font-size", "12px")
-        //   .attr("font-weight", "bold");
     //define some canvas variables
-
     var margin = {top: 30, right: 10, bottom: 20, left: 140},
         width = 960 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
@@ -73,10 +69,10 @@ TimeLineVis.prototype.initVis = function () {
     var minDate = d3.min(self.data, function(d){return d.Year;});
     var maxDate = d3.max(self.data, function(d){return d.Year;});
     maxDate = (+maxDate + 1).toString();
-    x = d3.time.scale()
+
+    var x = d3.time.scale()
         .domain([new Date(minDate), new Date(maxDate)])
         .range([0, width]);
-    // x.domain(d3.extent(self.data, function(d) { return new Date(d.Year; }));
 
     var y = d3.scale.linear()
         .domain([0,10])
@@ -110,10 +106,6 @@ TimeLineVis.prototype.initVis = function () {
         .orient("left")
         .ticks(10)
         .tickSize(-width, 3);
-        //.tickFormat("");
-
-
-    //var rectWidth = width/(2*self.data.length);
 
     self.svg.selectAll(".dot")
             .data(self.data)
@@ -136,33 +128,8 @@ TimeLineVis.prototype.initVis = function () {
             .on("mouseout", mouseout)
             .on("mousemove", mousemove);
 
-    // var rects = self.svg.selectAll("g")
-    //     .data(self.data)
-    //     .enter()
-    //     .append("g")
-    //     .attr("transform", function(d,i){ return "translate(" + i*rectWidth + ", 0)" });
+    //self.svg.call(tip);
     
-    // rects.append("rect")
-    //     .attr("x", function(d) {
-    //         return x(new Date(d.Released).getFullYear()) 
-    //     })
-    //     .attr("y", 0 )
-    //     .attr('date', function(d) {
-    //         return new Date(d.Released).getFullYear()})
-    //     .attr("height",function(d){return y(d.imdbRating);})
-    //     .attr("width", rectWidth)
-    //     .style("fill", "steelblue");
-
-    // rects.transition().duration(500).ease("exp-in-out")
-    //     .attr("height", function (d) { return xRange(__Number_of_films_in_timerange__); })
-    //     .attr("width", 20);
-    
-
-    // rects.exit ()
-    //     .transition().duration(1000).ease("exp-in-out")
-    //     .attr("width", 0)
-    //     .remove ();
-
      // Add the X Axis
     self.svg.append("g")
         .attr("class", "x axis")
@@ -190,10 +157,18 @@ TimeLineVis.prototype.initVis = function () {
         //setTimeout (update, 2000)
 
     function mousemove(d) {
-        var movie = "movie"//dimensions.map(function(p) { return [d[p]][0]; })
-        div.text("Title: " + d.Title)
-                .style("left", (d3.event.pageX - 40) + "px")
-                .style("top", (d3.event.pageY - 35) + "px");
+        div .html("<a> <img src=" + d.Poster + "/> </a> <br/>" +
+            "<strong>Title: </strong>" + d.Title + "<br/>" +
+            "<strong>Plot: </strong>" + d.Plot + "<br/>" +
+             "<strong>Director: </strong>" + d.Director + "<br/>" +
+            "<strong>Released: </strong>"+ d.Released + "<br/>" +
+            "<strong>Rating: </strong>"+ d.imdbRating + "<br/>" +
+            "<strong>No. Votes: </strong>"+ d.imdbVotes + "<br/>")
+            .style("left", (d3.event.pageX) + "px")     
+            .style("top", (d3.event.pageY - 68) + "px");    
+        //div.text("Title: " + d.Title)
+          //      .style("left", (d3.event.pageX - 40) + "px")
+            //    .style("top", (d3.event.pageY - 35) + "px");
             // .text("Imdb Rating: " + d.imdbRating)
             //     .style("left", (d3.event.pageX - 30) + "px")
             //     .style("top", (d3.event.pageY - 35) + "px")
@@ -203,6 +178,9 @@ TimeLineVis.prototype.initVis = function () {
     }
 
     function mouseover() {
+        div.transition()        
+            .duration(500)      
+            .style("opacity", .9);
       div.style("display", "inline");
     }
 
@@ -210,39 +188,7 @@ TimeLineVis.prototype.initVis = function () {
         div.style("display", "none");
     }
 }
-    //==================================================
-    //      Pseudo code for bar chart creation
-    //==================================================
-    // 1) read in actorMovies 
-    //         var startDate = d3.min(actorMovies[i].year)
-    //         var endDate = d3.max(actorMovies[i].year)
-    //         var lowYear = Math.floor(startDate/10)*10;
-    //         var highYear Math.ceil(endDate/10)*10;
-
-    //         var binWidth = 5; //  years per bin
-    //         var nBins = (highYear - lowYear)/binWidth;  // number of rectangles on the graph
-
-    // 2) Define an array of size 'nBins' to record the number of movies are in each bin range
-    //     var nMovies[nbins] = {}
-
-    //     for i = 1; i<=nBins; i++
-    //         for j = 0; j <=actorMovies.length; j++
-    //         if actorMovies[j].year >= (lowYear + (i-1)*binWidth)&& actorMovies[j].year < (lowYear + i*binWidth)
-    //             nMovies[i-1] +=1;
-            
-    // 3) Create a bar chart with ordinal data -- have 'nBins' number of rectangles, each with [scaled] height equal to the number of movies in the
-    //     bin --> 'nMovies[i]' for each i=1,2,3...nBins.
-    //     - rect height based on yScale (nMovies[i])
-    //     - make the spacing between the bars reasonable.  scalable?
-
-    // 4) when user clicks on a bar, rescale to a new barchart with 'binWidth' number of ticks in x
-
-// };  // end TimeLineVis.prototype.initVis
-
-// Keys to use:   decade --> size bars based on number of films
-//                       --> Shade baded on average rating?
-//                 Individual years ->  Size on releases?
-
+  
 // Probably  won't need these
 
 TimeLineVis.prototype.wrangleData = function (_filterFunction) {
