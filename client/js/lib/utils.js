b076e26af3bab9f20b25ce8c2673bd57b3e08018
Brian Kimmig <brian.kimmig@gmail.com>
2015-12-04 10:14:12 -0700

    fixed up utils for errors

--- client/js/lib/utils.js
+++ client/js/lib/utils.js
@@ -19,49 +19,59 @@ removeUnreleasedMovies = function(movieData) {
 filterData = function(movieData, keys) {
     var i = 0;
     var data = [];
-
+    console.log(movieData)
     for (i=0; i<movieData.length; i++){
         var movie = movieData[i];
-        keys.forEach(function(key) {
-            if (movie[key] === 'NaN' || movie[key] === 'N/A' ) {
-                movie[key] = 0.01;
-            }
+        
+        if (! movie.Error) {
 
-            if (key === 'Year' && movie[key].length > 4) {
-                // take only the starting year
-                movie[key] = movie[key].slice(0,4);
-            }
-
-             if (key === 'Release') {
-                // Format movie release date for plotting on Timeline Vis chart
-                movie[key];
-            }
-
-            if (key === 'BoxOffice' && movie[key] !== 0.01) {
-                var boxoffice = movie[key]
-                if (boxoffice.indexOf(';') !== -1) {
-                    boxoffice = boxoffice.split(";")[1];
-                    boxoffice = "$" + boxoffice;
-                }
-                boxoffice = boxoffice.split("$")[1];
-                boxoffice = boxoffice.split("M")[0];
-
-                if (boxoffice.indexOf('k') !== -1) {
-                    boxoffice = boxoffice.split("k")[0] / 1000.
+            keys.forEach(function(key) {
+                if (movie[key] === 'NaN' || movie[key] === 'N/A' ) {
+                    movie[key] = 0.01;
                 }
 
-                movie[key] = boxoffice;
-            }
-
-            if(key === 'imdbVotes'){
-                var tmpVotes = movie[key].toString();
-                 //console.log(x, x.indexOf(","))   
-                if(tmpVotes.indexOf(",") > -1){
-                     movie[key] = parseFloat(tmpVotes.replace(',', ''));
+                if (key === 'Year') {
+                    if(movie[key]) {
+                        if ( movie[key].length > 4) {
+                            // take only the starting year
+                            movie[key] = movie[key].slice(0,4);
+                        }
+                    }
                 }
-            }
 
-        })
+                if (key === 'Release') {
+                    // Format movie release date for plotting on Timeline Vis chart
+                    movie[key];
+                }
+
+                if (key === 'BoxOffice' && movie[key] !== 0.01) {
+                    var boxoffice = movie[key]
+                    if (boxoffice.indexOf(';') !== -1) {
+                        boxoffice = boxoffice.split(";")[1];
+                        boxoffice = "$" + boxoffice;
+                    }
+                    boxoffice = boxoffice.split("$")[1];
+                    boxoffice = boxoffice.split("M")[0];
+
+                    if (boxoffice.indexOf('k') !== -1) {
+                        boxoffice = boxoffice.split("k")[0] / 1000.
+                    }
+
+                    movie[key] = boxoffice;
+                }
+
+                if(key === 'imdbVotes'){
+                    if (movie[key]) {
+                        var tmpVotes = movie[key].toString();
+                         //console.log(x, x.indexOf(","))   
+                        if(tmpVotes.indexOf(",") > -1){
+                             movie[key] = parseFloat(tmpVotes.replace(',', ''));
+                        }
+                    }
+                }
+
+            })
+        }
         
         // only want data if it is a movie 
         if (movie.Type === 'movie') {
